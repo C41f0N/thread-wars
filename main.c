26041a63703673a21474ddda18c3c70c1ea00df5
Sarim Ahmed <C41f0N@protonmail.com>
2025-03-28 13:34:53 +0500

    made code a little modular and commented it

--- main.c
+++ main.c
@@ -1,16 +1,9 @@
+#include "models.c"
 #include "raylib.h"
 #include <math.h>
-#include <stdio.h>
 #include <stdlib.h>
+
 #define PLAYERS_NUM 2
-
-typedef struct {
-  int pos_x, pos_y;
-  int size;
-  Color color;
-} Player;
-
-Color colors[] = {RED, YELLOW, BLUE, GREEN};
 
 /*
     Stores keymaps for each player
@@ -21,11 +14,13 @@ int controls[3][4] = {{KEY_W, KEY_A, KEY_S, KEY_D},
                       {KEY_T, KEY_F, KEY_G, KEY_H}};
 
 Player *initializePlayers() {
+  Color playerColors[] = {RED, YELLOW, BLUE, GREEN};
+
   Player *players = (Player *)calloc(sizeof(Player), PLAYERS_NUM);
 
   for (int i = 0; i < PLAYERS_NUM; i++) {
     players[i].size = 50;
-    players[i].color = colors[i % PLAYERS_NUM];
+    players[i].color = playerColors[i % PLAYERS_NUM];
     players[i].pos_x = 0 + i * (20 + players[i].size);
     players[i].pos_y = 0;
   }
@@ -58,15 +53,17 @@ void handleInput(Player *players, int speed) {
       direction.x = 1;
     }
 
-    // Normalizing direction
+    // Calculating unit vector (direction) of movement
     float magnitude =
         sqrtf(direction.x * direction.x + direction.y * direction.y);
     if (magnitude != 0) {
       direction = (Vector2){direction.x / magnitude, direction.y / magnitude};
     }
 
+    // Multiplying direction with speed to get velocity
     Vector2 velocity = {direction.x * speed, direction.y * speed};
 
+    // Moving the player via velocity
     players[i].pos_x += velocity.x;
     players[i].pos_y += velocity.y;
   }
@@ -77,11 +74,12 @@ typedef struct {
   Color color;
 } Circle;
 
-Circle *initCircles(int n, int bounds) {
+// Creates randomly placed/sized Circles
+Circle *initCircles(int circleNum, int bounds) {
 
-  Circle *circles = calloc(n, sizeof(Circle));
+  Circle *circles = calloc(circleNum, sizeof(Circle));
 
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < circleNum; i++) {
     circles[i].centerX = (rand() % (2 * GetScreenWidth())) - GetScreenWidth();
     circles[i].centerY = (rand() % (2 * GetScreenHeight())) - GetScreenHeight();
     circles[i].radius = rand() % 5;
@@ -91,10 +89,75 @@ Circle *initCircles(int n, int bounds) {
   return circles;
 }
 
-void drawCircles(Circle *circles, int n) {
-  for (int i = 0; i < n; i++) {
+void drawCircles(Circle *circles, int circleNum) {
+  for (int i = 0; i < circleNum; i++) {
     DrawCircle(circles[i].centerX, circles[i].centerY, circles[i].radius,
                circles[i].color);
+  }
+}
+
+void draw(Viewport *viewports, Player *players, Circle *circles,
+          int circleNum) {
+
+  // Drawing on every viewport
+  for (int i = 0; i < PLAYERS_NUM; i++) {
+
+    // Updating camera to follow player
+    viewports[i].camera->target =
+        (Vector2){(int)(players[i].pos_x), (int)(players[i].pos_y)};
+
+    // Setting to draw on the viewport's RenderTexture
+    BeginTextureMode(*viewports[i].renderTexture);
+
+    // Drawing with respect to the viewport's Camera
+    BeginMode2D(*viewports[i].camera);
+
+    // Start from a clean slate
+    ClearBackground(BLACK);
+
+    // Random shit because I needed a background
+    drawCircles(circles, circleNum);
+
+    // Drawing all players
+    for (int j = 0; j < PLAYERS_NUM; j++) {
+      // DrawRectangle(players[j].pos_x, players[j].pos_y, players[j].size,
+      //               players[j].size, players[j].color);
+      DrawRectangleRounded((Rectangle){players[j].pos_x, players[j].pos_y,
+                                       players[j].size, players[j].size},
+                           0.5, 1, players[j].color);
+    }
+
+    EndMode2D();
+    EndTextureMode();
+  }
+
+  BeginDrawing();
+  ClearBackground(BLACK);
+
+  // Drawing the prepared viewports to a single screen sized rectangle
+  for (int i = 0; i < PLAYERS_NUM; i++) {
+    DrawTextureRec(viewports[i].renderTexture->texture,
+                   (Rectangle){0, 0, viewports[i].renderTexture->texture.width,
+                               -viewports[i].renderTexture->texture.height},
+                   (Vector2){i * (int)(GetScreenWidth() / PLAYERS_NUM), 0},
+                   WHITE);
+  }
+
+  // Drawing line(s) between screens
+  for (int i = 1; i < PLAYERS_NUM; i++) {
+    DrawRectangle(i * (int)(GetScreenWidth() / PLAYERS_NUM) - 2, 0, 4,
+                  GetScreenHeight(), WHITE);
+  }
+
+  DrawFPS(0, 0);
+
+  EndDrawing();
+}
+
+void killViewports(Viewport *viewports) {
+  // Unloading all render textures out of the GPU
+  for (int i = 0; i < PLAYERS_NUM; i++) {
+    UnloadRenderTexture(*viewports[i].renderTexture);
   }
 }
 
@@ -107,76 +170,22 @@ int main(int argc, char **args) {
   }
 
   Player *players = initializePlayers();
-
-  // Init render textures
-  RenderTexture2D *renderTextures =
-      calloc(sizeof(RenderTexture2D), PLAYERS_NUM);
-
-  for (int i = 0; i < PLAYERS_NUM; i++) {
-    renderTextures[i] =
-        LoadRenderTexture(GetScreenWidth() / PLAYERS_NUM, GetScreenHeight());
-  }
-
-  // init cameras
-  Camera2D *cameras = calloc(sizeof(Camera2D), PLAYERS_NUM);
-
-  for (int i = 0; i < PLAYERS_NUM; i++) {
-    cameras[i].zoom = 1.0;
-    cameras[i].offset = (Vector2){(int)(renderTextures[i].texture.width / 2),
-                                  (int)(renderTextures[i].texture.height / 2)};
-  }
-
-  int n = 2000;
-  Circle *circles = initCircles(n, 2);
+  Viewport *viewports = initializeViewports(PLAYERS_NUM, players);
 
   SetTargetFPS(60);
 
+  int circleNum = 3000;
+  Circle *circles = initCircles(circleNum, 3);
+
   while (!WindowShouldClose()) {
-    handleInput(players, 4);
+    // Handling Player Input (Should be done as threads later on)
+    handleInput(players, 6);
 
-    for (int i = 0; i < PLAYERS_NUM; i++) {
-      BeginTextureMode(renderTextures[i]);
-
-      cameras[i].target =
-          (Vector2){(int)(players[i].pos_x), (int)(players[i].pos_y)};
-      ClearBackground(BLACK);
-
-      BeginMode2D(cameras[i]);
-      drawCircles(circles, n);
-
-      for (int j = 0; j < PLAYERS_NUM; j++) {
-        DrawRectangle(players[j].pos_x, players[j].pos_y, players[j].size,
-                      players[j].size, players[j].color);
-      }
-
-      EndMode2D();
-      EndTextureMode();
-    }
-
-    BeginDrawing();
-    ClearBackground(BLACK);
-    for (int i = 0; i < PLAYERS_NUM; i++) {
-      DrawTextureRec(renderTextures[i].texture,
-                     (Rectangle){0, 0, renderTextures[i].texture.width,
-                                 -renderTextures[i].texture.height},
-                     (Vector2){i * (int)(GetScreenWidth() / PLAYERS_NUM), 0},
-                     WHITE);
-    }
-
-    // Drawing line between screens
-    for (int i = 1; i < PLAYERS_NUM; i++) {
-      DrawRectangle(i * (int)(GetScreenWidth() / PLAYERS_NUM) - 2, 0, 4,
-                    GetScreenHeight(), WHITE);
-    }
-
-    DrawFPS(0, 0);
-
-    EndDrawing();
+    // Drawing everything to screen
+    draw(viewports, players, circles, circleNum);
   }
 
-  for (int i = 0; i < PLAYERS_NUM; i++) {
-    UnloadRenderTexture(renderTextures[i]);
-  }
+  killViewports(viewports);
 
   CloseWindow();
 
---
+++ models.c
@@ -0,0 +1,48 @@
+#include <raylib.h>
+#include <stdlib.h>
+
+// Player Struct
+typedef struct {
+  int pos_x, pos_y;
+  int size;
+  int health;
+  Color color;
+} Player;
+
+// Viewport Struct
+typedef struct {
+  Camera2D *camera;
+  RenderTexture2D *renderTexture;
+  Player *player;
+} Viewport;
+
+// Function to create and initialize viewports
+Viewport *initializeViewports(int numPlayers, Player *players) {
+
+  // Allocating in memory
+  RenderTexture2D *renderTextures = calloc(sizeof(RenderTexture2D), numPlayers);
+  Camera2D *cameras = calloc(sizeof(Camera2D), numPlayers);
+  Viewport *viewports = calloc(numPlayers, sizeof(Viewport));
+
+  for (int i = 0; i < numPlayers; i++) {
+    // Creating render textures
+    renderTextures[i] =
+        LoadRenderTexture(GetScreenWidth() / numPlayers, GetScreenHeight());
+
+    // Setting up cameras
+    cameras[i].zoom = 1.0;
+    cameras[i].offset = (Vector2){(int)(renderTextures[i].texture.width / 2),
+                                  (int)(renderTextures[i].texture.height / 2)};
+
+    // Setting a render texture to viewport
+    viewports[i].renderTexture = &renderTextures[i];
+
+    // Setting a camera to viewport
+    viewports[i].camera = &cameras[i];
+
+    // Setting a player to viewport
+    viewports[i].player = &players[i];
+  }
+
+  return viewports;
+}
