b96f88336c82030d2e965bd78c08360b93bcda28
Sarim Ahmed <C41f0N@protonmail.com>
2025-03-30 13:22:12 +0500

    zero division fixed

--- lib/vector_ops.c
+++ lib/vector_ops.c
@@ -9,10 +9,19 @@ float getDistanceBetweenVectors(Vector2 v1, Vector2 v2) {
 float getVectorMagnitude(Vector2 v) { return sqrtf(pow(v.x, 2) + pow(v.y, 2)); }
 
 Vector2 normalizeVector2(Vector2 v) {
-  return (Vector2){v.x / getVectorMagnitude(v), v.y / getVectorMagnitude(v)};
+  float magnitude = getVectorMagnitude(v);
+  if (magnitude == 0) {
+    return (Vector2){0, 0};
+  }
+  return (Vector2){v.x / magnitude, v.y / magnitude};
 }
 
 Vector2 getDirectionVector2s(Vector2 v1, Vector2 v2) {
+  float distance = getDistanceBetweenVectors(v1, v2);
+  if (distance == 0) {
+    return (Vector2){0, 0};
+  }
+
   return (Vector2){(v2.x - v1.x) / getDistanceBetweenVectors(v1, v2),
                    (v2.y - v1.y) / getDistanceBetweenVectors(v1, v2)};
 }
